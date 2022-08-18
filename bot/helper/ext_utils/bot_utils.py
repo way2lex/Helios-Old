8d137fc17654165be3f8306c6b70ef3ffa85baef
way2lex <111517701+way2lex@users.noreply.github.com>
2022-08-18 10:41:25 -0700

    Initialize h-code

---
+++ bot/helper/ext_utils/bot_utils.py
@@ -0,0 +1,356 @@
+from re import match as re_match, findall as re_findall
+from threading import Thread, Event
+from time import time
+from math import ceil
+from html import escape
+from psutil import disk_usage, cpu_percent, swap_memory, cpu_count, virtual_memory, net_io_counters, boot_time
+from requests import head as rhead
+from urllib.request import urlopen
+from telegram import InlineKeyboardMarkup
+from telegram.ext import CallbackQueryHandler
+from bot.helper.telegram_helper.bot_commands import BotCommands
+from bot import download_dict, download_dict_lock, STATUS_LIMIT, botStartTime, DOWNLOAD_DIR, dispatcher
+from bot.helper.telegram_helper.button_build import ButtonMaker
+
+MAGNET_REGEX = r"magnet:\?xt=urn:btih:[a-zA-Z0-9]*"
+
+URL_REGEX = r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+"
+
+COUNT = 0
+PAGE_NO = 1
+
+
+class MirrorStatus:
+    STATUS_UPLOADING = "ᴜᴘʟᴏᴀᴅɪɴɢ...📤"
+    STATUS_DOWNLOADING = "ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ...📥"
+    STATUS_CLONING = "ᴄʟᴏɴɪɴɢ...♻️"
+    STATUS_WAITING = "ǫᴜᴇᴜᴇᴅ...💤"
+    STATUS_FAILED = "ғᴀɪʟᴇᴅ 🚫. ᴄʟᴇᴀɴɪɴɢ ᴅᴏᴡɴʟᴏᴀᴅs..."
+    STATUS_PAUSE = "ᴘᴀᴜsᴇᴅ...⛔️"
+    STATUS_ARCHIVING = "ᴀʀᴄʜɪᴠɪɴɢ...🔐"
+    STATUS_EXTRACTING = "ᴇxᴛʀᴀᴄᴛɪɴɢ...📂"
+    STATUS_SPLITTING = "sᴘʟɪᴛᴛɪɴɢ...✂️"
+    STATUS_CHECKING = "ᴄʜᴇᴄᴋɪɴɢ ᴜᴘ...📝"
+    STATUS_SEEDING = "sᴇᴇᴅɪɴɢ...🌧"
+
+class EngineStatus:
+    STATUS_ARIA = "ᴀʀɪᴀ2ᴄ"
+    STATUS_GDRIVE = "ɢᴏᴏɢʟᴇ ᴀᴘɪ"
+    STATUS_MEGA = "ᴍᴇɢᴀ ᴀᴘɪ"
+    STATUS_QB = "ǫʙɪᴛᴛᴏʀʀᴇɴᴛ"
+    STATUS_TG = "ᴘʏʀᴏɢʀᴀᴍ - ᴜᴘʟᴏᴀᴅɪɴɢ ᴛᴏ ᴛɢ"
+    STATUS_YT = "ʏᴛ-ᴅʟᴘ"
+    STATUS_EXT = "ᴇxᴛʀᴀᴄᴛ | ᴘᴇxᴛʀᴀᴄᴛ"
+    STATUS_SPLIT = "ғғᴍᴘᴇɢ"
+    STATUS_ZIP = "7ᴢ"
+
+SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+
+
+class setInterval:
+    def __init__(self, interval, action):
+        self.interval = interval
+        self.action = action
+        self.stopEvent = Event()
+        thread = Thread(target=self.__setInterval)
+        thread.start()
+
+    def __setInterval(self):
+        nextTime = time() + self.interval
+        while not self.stopEvent.wait(nextTime - time()):
+            nextTime += self.interval
+            self.action()
+
+    def cancel(self):
+        self.stopEvent.set()
+
+def get_readable_file_size(size_in_bytes) -> str:
+    if size_in_bytes is None:
+        return '0B'
+    index = 0
+    while size_in_bytes >= 1024:
+        size_in_bytes /= 1024
+        index += 1
+    try:
+        return f'{round(size_in_bytes, 2)}{SIZE_UNITS[index]}'
+    except IndexError:
+        return 'File too large'
+
+def getDownloadByGid(gid):
+    with download_dict_lock:
+        for dl in list(download_dict.values()):
+            status = dl.status()
+            if (
+                status
+                not in [
+                    MirrorStatus.STATUS_ARCHIVING,
+                    MirrorStatus.STATUS_EXTRACTING,
+                    MirrorStatus.STATUS_SPLITTING,
+                ]
+                and dl.gid() == gid
+            ):
+                return dl
+    return None
+
+def getAllDownload(req_status: str):
+    with download_dict_lock:
+        for dl in list(download_dict.values()):
+            status = dl.status()
+            if status not in [MirrorStatus.STATUS_ARCHIVING, MirrorStatus.STATUS_EXTRACTING, MirrorStatus.STATUS_SPLITTING] and dl:
+                if req_status == 'down' and (status not in [MirrorStatus.STATUS_SEEDING,
+                                                            MirrorStatus.STATUS_UPLOADING,
+                                                            MirrorStatus.STATUS_CLONING]):
+                    return dl
+                elif req_status == 'up' and status == MirrorStatus.STATUS_UPLOADING:
+                    return dl
+                elif req_status == 'clone' and status == MirrorStatus.STATUS_CLONING:
+                    return dl
+                elif req_status == 'seed' and status == MirrorStatus.STATUS_SEEDING:
+                    return dl
+                elif req_status == 'all':
+                    return dl
+    return None
+
+def get_progress_bar_string(status):
+    completed = status.processed_bytes() / 8
+    total = status.size_raw() / 8
+    p = 0 if total == 0 else round(completed * 100 / total)
+    p = min(max(p, 0), 100)
+    cFull = p // 8
+    p_str = '▰' * cFull
+    p_str += '▱' * (12 - cFull)
+    p_str = f"[{p_str}]"
+    return p_str
+
+def get_readable_message():
+    with download_dict_lock:
+        msg = ""
+        if STATUS_LIMIT is not None:
+            tasks = len(download_dict)
+            global pages
+            pages = ceil(tasks/STATUS_LIMIT)
+            if PAGE_NO > pages and pages != 0:
+                globals()['COUNT'] -= STATUS_LIMIT
+                globals()['PAGE_NO'] -= 1
+        for index, download in enumerate(list(download_dict.values())[COUNT:], start=1):
+            msg += f"<b>ɴᴀᴍᴇ:</b> <code>{escape(str(download.name()))}</code>"
+            msg += f"\n<b>sᴛᴀᴛᴜs:</b> <i>{download.status()}</i>\n<b>ᴇɴɢɪɴᴇ:</b> {download.eng()}"
+            if download.status() not in [
+                MirrorStatus.STATUS_ARCHIVING,
+                MirrorStatus.STATUS_EXTRACTING,
+                MirrorStatus.STATUS_SPLITTING,
+                MirrorStatus.STATUS_SEEDING,
+            ]:
+                msg += f"\n{get_progress_bar_string(download)} {download.progress()}"
+                if download.status() == MirrorStatus.STATUS_CLONING:
+                    msg += f"\n<b>ᴄʟᴏɴᴇᴅ:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                elif download.status() == MirrorStatus.STATUS_UPLOADING:
+                    msg += f"\n<b>ᴜᴘʟᴏᴀᴅᴇᴅ:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                else:
+                    msg += f"\n<b>ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                msg += f"\n<b>sᴘᴇᴇᴅ:</b> {download.speed()} | <b>ᴇᴛᴀ:</b> {download.eta()}"
+                msg += f"\n<b>ᴛɪᴍᴇ ᴇʟᴀᴘsᴇᴅ: </b>{get_readable_time(time() - download.message.date.timestamp())}"
+                msg += f"\n<b>ᴇɴɢɪɴᴇ:</b> {download.eng()}"
+                try:
+                   msg += f"\n<b>sᴇᴇᴅᴇʀs:</b> {download.aria_download().num_seeders}" \
+                           f" | <b>ᴘᴇᴇʀs:</b> {download.aria_download().connections}"
+                except:
+                    pass
+                try:
+                    msg += f"\n<b>sᴇᴇᴅᴇʀs:</b> {download.torrent_info().num_seeds}" \
+                           f" | <b>ʟᴇᴇᴄʜᴇʀs:</b> {download.torrent_info().num_leechs}"
+                except:
+                    pass
+                if download.message.chat.type != 'private':
+                    try:
+                        chatid = str(download.message.chat.id)[4:]
+                        msg += f'\n<b>sᴏᴜʀᴄᴇ: </b><a href="https://t.me/c/{chatid}/{download.message.message_id}">{download.message.from_user.first_name}</a> | <b>Id :</b> <code>{download.message.from_user.id}</code>'
+                    except:
+                        pass
+                else:
+                     msg += f'\n<b>ᴜsᴇʀ:</b> ️<code>{download.message.from_user.first_name}</code> | <b>ɪᴅ:</b> <code>{download.message.from_user.id}</code>'
+                msg += f"\n<b>ᴛᴏ sᴛᴏᴘ: </b><code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+            elif download.status() == MirrorStatus.STATUS_SEEDING:
+                msg += f"\n<b>sɪᴢᴇ: </b>{download.size()}"
+                msg += f"\n<b>sᴘᴇᴇᴅ: </b>{get_readable_file_size(download.torrent_info().upspeed)}/s"
+                msg += f"\n<b>ᴇɴɢɪɴᴇ: </b> {download.eng()}"
+                msg += f" | <b>ᴜᴘʟᴏᴀᴅᴇᴅ: </b>{get_readable_file_size(download.torrent_info().uploaded)}"
+                msg += f"\n<b>ʀᴀᴛɪᴏ: </b>{round(download.torrent_info().ratio, 3)}"
+                msg += f" | <b>ᴛɪᴍᴇ: </b>{get_readable_time(download.torrent_info().seeding_time)}"
+                msg += f"\n<code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+            else:
+                msg += f"\n<b>sɪᴢᴇ: </b>{download.size()}"
+            msg += "\n\n"
+            if STATUS_LIMIT is not None and index == STATUS_LIMIT:
+                break
+        bmsg = f"<b>ᴄᴘᴜ:</b> {cpu_percent()}% | <b>ғʀᴇᴇ:</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
+        bmsg += f"\n<b>ʀᴀᴍ:</b> {virtual_memory().percent}% | <b>ᴜᴘᴛɪᴍᴇ:</b> {get_readable_time(time() - botStartTime)}"
+        dlspeed_bytes = 0
+        upspeed_bytes = 0
+        for download in list(download_dict.values()):
+            spd = download.speed()
+            if download.status() == MirrorStatus.STATUS_DOWNLOADING:
+                if 'K' in spd:
+                    dlspeed_bytes += float(spd.split('K')[0]) * 1024
+                elif 'M' in spd:
+                    dlspeed_bytes += float(spd.split('M')[0]) * 1048576
+            elif download.status() == MirrorStatus.STATUS_UPLOADING:
+                if 'KB/s' in spd:
+                    upspeed_bytes += float(spd.split('K')[0]) * 1024
+                elif 'MB/s' in spd:
+                    upspeed_bytes += float(spd.split('M')[0]) * 1048576
+        bmsg += f"\n<b>DL:</b> {get_readable_file_size(dlspeed_bytes)}/s | <b>UL:</b> {get_readable_file_size(upspeed_bytes)}/s"
+        buttons = ButtonMaker()
+        buttons.sbutton("sᴛᴀᴛɪsᴛɪᴄs", str(FOUR))
+        sbutton = InlineKeyboardMarkup(buttons.build_menu(1))
+        if STATUS_LIMIT is not None and tasks > STATUS_LIMIT:
+            buttons = ButtonMaker()
+            buttons.sbutton("ᴘʀᴇᴠɪᴏᴜs", "status pre")
+            buttons.sbutton(f"{PAGE_NO}/{pages}", str(THREE))
+            buttons.sbutton("ɴᴇxᴛ", "status nex")
+            buttons.sbutton("sᴛᴀᴛɪsᴛɪᴄs", str(FOUR))
+            button = InlineKeyboardMarkup(buttons.build_menu(3))
+            return msg + bmsg, button
+        return msg + bmsg, sbutton
+
+def turn(data):
+    try:
+        with download_dict_lock:
+            global COUNT, PAGE_NO
+            if data[1] == "nex":
+                if PAGE_NO == pages:
+                    COUNT = 0
+                    PAGE_NO = 1
+                else:
+                    COUNT += STATUS_LIMIT
+                    PAGE_NO += 1
+            elif data[1] == "pre":
+                if PAGE_NO == 1:
+                    COUNT = STATUS_LIMIT * (pages - 1)
+                    PAGE_NO = pages
+                else:
+                    COUNT -= STATUS_LIMIT
+                    PAGE_NO -= 1
+        return True
+    except:
+        return False
+
+def get_readable_time(seconds: int) -> str:
+    result = ''
+    (days, remainder) = divmod(seconds, 86400)
+    days = int(days)
+    if days != 0:
+        result += f'{days}d'
+    (hours, remainder) = divmod(remainder, 3600)
+    hours = int(hours)
+    if hours != 0:
+        result += f'{hours}h'
+    (minutes, seconds) = divmod(remainder, 60)
+    minutes = int(minutes)
+    if minutes != 0:
+        result += f'{minutes}m'
+    seconds = int(seconds)
+    result += f'{seconds}s'
+    return result
+
+def is_url(url: str):
+    url = re_findall(URL_REGEX, url)
+    return bool(url)
+
+def is_gdrive_link(url: str):
+    return "drive.google.com" in url
+
+def is_gdtot_link(url: str):
+    url = re_match(r'https?://.+\.gdtot\.\S+', url)
+    return bool(url)
+
+def is_appdrive_link(url: str):
+    url = re_match(r'https?://(?:\S*\.)?(?:appdrive|driveapp)\.in/\S+', url)
+    return bool(url)
+def is_mega_link(url: str):
+    return "mega.nz" in url or "mega.co.nz" in url
+
+def get_mega_link_type(url: str):
+    if "folder" in url:
+        return "folder"
+    elif "file" in url:
+        return "file"
+    elif "/#F!" in url:
+        return "folder"
+    return "file"
+
+def is_magnet(url: str):
+    magnet = re_findall(MAGNET_REGEX, url)
+    return bool(magnet)
+
+def new_thread(fn):
+    """To use as decorator to make a function call threaded.
+    Needs import
+    from threading import Thread"""
+
+    def wrapper(*args, **kwargs):
+        thread = Thread(target=fn, args=args, kwargs=kwargs)
+        thread.start()
+        return thread
+
+    return wrapper
+
+def get_content_type(link: str) -> str:
+    try:
+        res = rhead(link, allow_redirects=True, timeout=5, headers = {'user-agent': 'Wget/1.12'})
+        content_type = res.headers.get('content-type')
+    except:
+        try:
+            res = urlopen(link, timeout=5)
+            info = res.info()
+            content_type = info.get_content_type()
+        except:
+            content_type = None
+    return content_type
+
+ONE, TWO, THREE, FOUR = range(4)
+def pop_up_stats(update, context):
+    query = update.callback_query
+    stats = bot_sys_stats()
+    query.answer(text=stats, show_alert=True)
+def bot_sys_stats():
+    currentTime = get_readable_time(time() - botStartTime)
+    cpu = cpu_percent(interval=0.5)
+    memory = virtual_memory()
+    mem_p = memory.percent
+    total, used, free, disk = disk_usage('/')
+    total = get_readable_file_size(total)
+    used = get_readable_file_size(used)
+    free = get_readable_file_size(free)
+    sent = get_readable_file_size(net_io_counters().bytes_sent)
+    recv = get_readable_file_size(net_io_counters().bytes_recv)
+    num_active = 0
+    num_upload = 0
+    num_split = 0
+    num_extract = 0
+    num_archi = 0
+    tasks = len(download_dict)
+    for stats in list(download_dict.values()):
+       if stats.status() == MirrorStatus.STATUS_DOWNLOADING:
+                num_active += 1
+       if stats.status() == MirrorStatus.STATUS_UPLOADING:
+                num_upload += 1
+       if stats.status() == MirrorStatus.STATUS_ARCHIVING:
+                num_archi += 1
+       if stats.status() == MirrorStatus.STATUS_EXTRACTING:
+                num_extract += 1
+       if stats.status() == MirrorStatus.STATUS_SPLITTING:
+                num_split += 1
+    stats = f"""
+BOT UPTIME: {currentTime}\n
+CPU : {cpu}% || RAM : {mem_p}%\n
+USED : {used} || FREE :{free}
+SENT : {sent} || RECV : {recv}\n
+ONGOING TASKS:
+DL: {num_active} || UP : {num_upload} || SPLIT : {num_split}
+ZIP : {num_archi} || UNZIP : {num_extract} || TOTAL : {tasks} 
+"""
+    return stats
+dispatcher.add_handler(
+    CallbackQueryHandler(pop_up_stats, pattern="^" + str(FOUR) + "$")
+)
---
+++ bot/modules/authorize.py
@@ -0,0 +1,253 @@
+from bot import AUTHORIZED_CHATS, SUDO_USERS, dispatcher, DB_URI, LEECH_LOG
+from bot.helper.telegram_helper.message_utils import sendMessage
+from telegram.ext import CommandHandler
+from bot.helper.telegram_helper.filters import CustomFilters
+from bot.helper.telegram_helper.bot_commands import BotCommands
+from bot.helper.ext_utils.db_handler import DbManger
+
+
+def authorize(update, context):
+    reply_message = None
+    message_ = None
+    reply_message = update.message.reply_to_message
+    message_ = update.message.text.split(' ')
+    if len(message_) == 2:
+        user_id = int(message_[1])
+        if user_id in AUTHORIZED_CHATS:
+            msg = 'ᴜsᴇʀ ᴀʟʀᴇᴀᴅʏ ᴀᴜᴛʜᴏʀɪᴢᴇᴅ!'
+        elif DB_URI is not None:
+            msg = DbManger().user_auth(user_id)
+            AUTHORIZED_CHATS.add(user_id)
+        else:
+            AUTHORIZED_CHATS.add(user_id)
+            msg = 'ᴜsᴇʀ ᴀᴜᴛʜᴏʀɪᴢᴇᴅ!'
+    elif reply_message is None:
+        # Trying to authorize a chat
+        chat_id = update.effective_chat.id
+        if chat_id in AUTHORIZED_CHATS:
+            msg = 'ᴄʜᴀᴛ ᴀʟʀᴇᴀᴅʏ ᴀᴜᴛʜᴏʀɪᴢᴇᴅ!'
+        elif DB_URI is not None:
+            msg = DbManger().user_auth(chat_id)
+            AUTHORIZED_CHATS.add(chat_id)
+        else:
+            AUTHORIZED_CHATS.add(chat_id)
+            msg = 'ᴄʜᴀᴛ ᴀᴜᴛʜᴏʀɪᴢᴇᴅ!'
+    else:
+        # Trying to authorize someone by replying
+        user_id = reply_message.from_user.id
+        if user_id in AUTHORIZED_CHATS:
+            msg = 'ᴜsᴇʀ ᴀʟʀᴇᴀᴅʏ ᴀᴜᴛʜᴏʀɪᴢᴇᴅ!'
+        elif DB_URI is not None:
+            msg = DbManger().user_auth(user_id)
+            AUTHORIZED_CHATS.add(user_id)
+        else:
+            AUTHORIZED_CHATS.add(user_id)
+            msg = 'ᴜsᴇʀ ᴀᴜᴛʜᴏʀɪᴢᴇᴅ!'
+    sendMessage(msg, context.bot, update.message)
+
+def unauthorize(update, context):
+    reply_message = None
+    message_ = None
+    reply_message = update.message.reply_to_message
+    message_ = update.message.text.split(' ')
+    if len(message_) == 2:
+        user_id = int(message_[1])
+        if user_id in AUTHORIZED_CHATS:
+            if DB_URI is not None:
+                msg = DbManger().user_unauth(user_id)
+            else:
+                msg = 'ᴜsᴇʀ ᴜɴᴀᴜᴛʜᴏʀɪᴢᴇᴅ!'
+            AUTHORIZED_CHATS.remove(user_id)
+        else:
+            msg = 'ᴜsᴇʀ ᴀʟʀᴇᴀᴅʏ ᴜɴᴀᴜᴛʜᴏʀɪᴢᴇᴅ!'
+    elif reply_message is None:
+        # Trying to unauthorize a chat
+        chat_id = update.effective_chat.id
+        if chat_id in AUTHORIZED_CHATS:
+            if DB_URI is not None:
+                msg = DbManger().user_unauth(chat_id)
+            else:
+                msg = 'ᴄʜᴀᴛ ᴜɴᴀᴜᴛʜᴏʀɪᴢᴇᴅ!'
+            AUTHORIZED_CHATS.remove(chat_id)
+        else:
+            msg = 'ᴄʜᴀᴛ ᴀʟʀᴇᴀᴅʏ ᴜɴᴀᴜᴛʜᴏʀɪᴢᴇᴅ!!'
+    else:
+        # Trying to authorize someone by replying
+        user_id = reply_message.from_user.id
+        if user_id in AUTHORIZED_CHATS:
+            if DB_URI is not None:
+                msg = DbManger().user_unauth(user_id)
+            else:
+                msg = 'ᴜsᴇʀ ᴜɴᴀᴜᴛʜᴏʀɪᴢᴇᴅ!'
+            AUTHORIZED_CHATS.remove(user_id)
+        else:
+            msg = 'ᴜsᴇʀ ᴀʟʀᴇᴀᴅʏ ᴜɴᴀᴜᴛʜᴏʀɪᴢᴇᴅ!'
+    sendMessage(msg, context.bot, update.message)
+
+def addleechlog(update, context):
+    reply_message = None
+    message_ = None
+    reply_message = update.message.reply_to_message
+    message_ = update.message.text.split(' ')
+    if len(message_) == 2:
+        user_id = int(message_[1])
+        if user_id in LEECH_LOG:
+            msg = 'ᴜsᴇʀ ᴀʟʀᴇᴀᴅʏ ɪɴ ʟᴇᴇᴄʜ ʟᴏɢ!'
+        elif DB_URI is not None:
+            msg = DbManger().addleech_log(user_id)
+            LEECH_LOG.add(user_id)
+        else:
+            LEECH_LOG.add(user_id)
+            msg = 'ᴜsᴇʀ ᴀᴅᴅᴇᴅ ɪɴ ʟᴇᴇᴄʜ ʟᴏɢ!'
+    elif reply_message is None:
+        # Trying to authorize a chat
+        chat_id = update.effective_chat.id
+        if chat_id in LEECH_LOG:
+            msg = 'ᴄʜᴀᴛ ᴀʟʀᴇᴀᴅʏ ɪɴ ʟᴇᴇᴄʜ ʟᴏɢ!'
+        elif DB_URI is not None:
+            msg = DbManger().addleech_log(chat_id)
+            LEECH_LOG.add(chat_id)
+        else:
+            LEECH_LOG.add(chat_id)
+            msg = 'ᴄʜᴀᴛ ᴀᴅᴅᴇᴅ ᴛᴏ ʟᴇᴇᴄʜ ʟᴏɢ!'
+    else:
+        # Trying to authorize someone by replying
+        user_id = reply_message.from_user.id
+        if user_id in LEECH_LOG:
+            msg = 'ᴄʜᴀᴛ ᴀʟʀᴇᴀᴅʏ ɪɴ ʟᴇᴇᴄʜ ʟᴏɢ!'
+        elif DB_URI is not None:
+            msg = DbManger().addleech_log(user_id)
+            LEECH_LOG.add(user_id)
+        else:
+            LEECH_LOG.add(user_id)
+            msg = 'ᴄʜᴀᴛ ᴀᴅᴅᴇᴅ ᴛᴏ ʟᴇᴇᴄʜ ʟᴏɢ!'
+    sendMessage(msg, context.bot, update.message)
+
+
+def rmleechlog(update, context):
+    reply_message = None
+    message_ = None
+    reply_message = update.message.reply_to_message
+    message_ = update.message.text.split(' ')
+    if len(message_) == 2:
+        user_id = int(message_[1])
+        if user_id in LEECH_LOG:
+            if DB_URI is not None:
+                msg = DbManger().rmleech_log(user_id)
+            else:
+                msg = 'ᴜsᴇʀ ʀᴇᴍᴏᴠᴇ ғʀᴏᴍ ʟᴇᴇᴄʜ ʟᴏɢ!'
+            LEECH_LOG.remove(user_id)
+        else:
+            msg = 'ᴜsᴇʀ ᴅᴏᴇs ɴᴏᴛ ᴇxɪsᴛ ɪɴ ʟᴇᴇᴄʜ ʟᴏɢ!'
+    elif reply_message is None:
+        # Trying to unauthorize a chat
+        chat_id = update.effective_chat.id
+        if chat_id in LEECH_LOG:
+            if DB_URI is not None:
+                msg = DbManger().rmleech_log(chat_id)
+            else:
+                msg = 'ᴄʜᴀᴛ ʀᴇᴍᴏᴠᴇᴅ ғʀᴏᴍ ʟᴇᴇᴄʜ ʟᴏɢ!'
+            LEECH_LOG.remove(chat_id)
+        else:
+            msg = 'ᴄʜᴀᴛ ᴅᴏᴇs ɴᴏᴛ ᴇxɪsᴛ ɪɴ ʟᴇᴇᴄʜ ʟᴏɢ!'
+    else:
+        # Trying to authorize someone by replying
+        user_id = reply_message.from_user.id
+        if user_id in LEECH_LOG:
+            if DB_URI is not None:
+                msg = DbManger().rmleech_log(user_id)
+            else:
+                msg = 'ᴜsᴇʀ ʀᴇᴍᴏᴠᴇ ғʀᴏᴍ ʟᴇᴇᴄʜ ʟᴏɢ!'
+            LEECH_LOG.remove(user_id)
+        else:
+            msg = 'ᴜsᴇʀ ᴅᴏᴇs ɴᴏᴛ ᴇxɪsᴛ ɪɴ ʟᴇᴇᴄʜ ʟᴏɢ!'
+    sendMessage(msg, context.bot, update.message)
+
+def addSudo(update, context):
+    reply_message = None
+    message_ = None
+    reply_message = update.message.reply_to_message
+    message_ = update.message.text.split(' ')
+    if len(message_) == 2:
+        user_id = int(message_[1])
+        if user_id in SUDO_USERS:
+            msg = 'ᴀʟʀᴇᴀᴅʏ sᴜᴅᴏ!'
+        elif DB_URI is not None:
+            msg = DbManger().user_addsudo(user_id)
+            SUDO_USERS.add(user_id)
+        else:
+            SUDO_USERS.add(user_id)
+            msg = 'ᴘʀᴏᴍᴏᴛᴇᴅ ᴛᴏ sᴜᴅᴏ'
+    elif reply_message is None:
+        msg = "ɢɪᴠᴇ ɪᴅ ᴏʀ ʀᴇᴘʟʏ ᴛᴏ ᴍᴇssᴀɢᴇ ᴛʜᴇ ᴏɴᴇ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴘʀᴏᴍᴏᴛᴇ."
+    else:
+        # Trying to authorize someone by replying
+        user_id = reply_message.from_user.id
+        if user_id in SUDO_USERS:
+            msg = 'ᴀʟʀᴇᴀᴅʏ sᴜᴅᴏ!'
+        elif DB_URI is not None:
+            msg = DbManger().user_addsudo(user_id)
+            SUDO_USERS.add(user_id)
+        else:
+            SUDO_USERS.add(user_id)
+            msg = 'ᴘʀᴏᴍᴏᴛᴇᴅ ᴛᴏ sᴜᴅᴏ'
+    sendMessage(msg, context.bot, update.message)
+
+def removeSudo(update, context):
+    reply_message = None
+    message_ = None
+    reply_message = update.message.reply_to_message
+    message_ = update.message.text.split(' ')
+    if len(message_) == 2:
+        user_id = int(message_[1])
+        if user_id in SUDO_USERS:
+            if DB_URI is not None:
+                msg = DbManger().user_rmsudo(user_id)
+            else:
+                msg = 'ᴅᴇᴍᴏᴛᴇᴅ!'
+            SUDO_USERS.remove(user_id)
+        else:
+            msg = 'ɴᴏᴛ sᴜᴅᴏ ᴜsᴇʀ ᴛᴏ ᴅᴇᴍᴏᴛᴇᴅ!'
+    elif reply_message is None:
+        msg = "ɢɪᴠᴇ ɪᴅ ᴏʀ ʀᴇᴘʟʏ ᴛᴏ ᴍᴇssᴀɢᴇ ᴛʜᴇ ᴏɴᴇ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ʀᴇᴍᴏᴠᴇ ғʀᴏᴍ sᴜᴅᴏ."
+    else:
+        user_id = reply_message.from_user.id
+        if user_id in SUDO_USERS:
+            if DB_URI is not None:
+                msg = DbManger().user_rmsudo(user_id)
+            else:
+                msg = 'ᴅᴇᴍᴏᴛᴇᴅ!'
+            SUDO_USERS.remove(user_id)
+        else:
+            msg = 'ɴᴏᴛ sᴜᴅᴏ ᴜsᴇʀ ᴛᴏ ᴅᴇᴍᴏᴛᴇᴅ!'
+    sendMessage(msg, context.bot, update.message)
+
+def sendAuthChats(update, context):
+    user = sudo = leechlog = ''
+    user += '\n'.join(f"<code>{uid}</code>" for uid in AUTHORIZED_CHATS)
+    sudo += '\n'.join(f"<code>{uid}</code>" for uid in SUDO_USERS)
+    leechlog += '\n'.join(f"<code>{uid}</code>" for uid in LEECH_LOG)
+    sendMessage(f'<b><u>Authorized Chats:</u></b>\n{user}\n<b><u>Sudo Users:</u></b>\n{sudo}\n<b><u>Leech Log:</u></b>\n{leechlog}\n', context.bot, update.message)
+
+
+send_auth_handler = CommandHandler(command=BotCommands.AuthorizedUsersCommand, callback=sendAuthChats,
+                                    filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
+authorize_handler = CommandHandler(command=BotCommands.AuthorizeCommand, callback=authorize,
+                                    filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
+unauthorize_handler = CommandHandler(command=BotCommands.UnAuthorizeCommand, callback=unauthorize,
+                                    filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
+addsudo_handler = CommandHandler(command=BotCommands.AddSudoCommand, callback=addSudo,
+                                    filters=CustomFilters.owner_filter, run_async=True)
+removesudo_handler = CommandHandler(command=BotCommands.RmSudoCommand, callback=removeSudo,
+                                    filters=CustomFilters.owner_filter, run_async=True)
+addleechlog_handler = CommandHandler(command=BotCommands.AddleechlogCommand, callback=addleechlog,
+                                    filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
+rmleechlog_handler = CommandHandler(command=BotCommands.RmleechlogCommand, callback=rmleechlog,
+                                    filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
+dispatcher.add_handler(send_auth_handler)
+dispatcher.add_handler(authorize_handler)
+dispatcher.add_handler(unauthorize_handler)
+dispatcher.add_handler(addsudo_handler)
+dispatcher.add_handler(removesudo_handler)
+dispatcher.add_handler(addleechlog_handler)
+dispatcher.add_handler(rmleechlog_handler)
